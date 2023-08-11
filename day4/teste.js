fdf9fb0f0ae9215cc3ce9a91f5a9c671e94fd17e
Anderson-23 <MichelAnderson.Silva23@gmail.com>
2023-08-10 22:38:12 -0300

    up

--- day4/teste.js
+++ day4/teste.js
@@ -1,12 +1,12 @@
 
 const teste = document.querySelector(".principal")
-
+const teste2 = document.querySelector(".submenu")
 
     
 
 function clicar(){
 
 teste.classList.toggle("mexer")
-
+teste2.classList.toggle("mexer2")
 
 }
